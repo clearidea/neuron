153cd101898ee71d2f693d572f5ed9748531f27f
Jeremiah Yoder <jeremiahyoder@Jeremiahs-MacBook-Pro.local>
2017-02-22 09:18:49 -0500

    added Time Validator unit test

---
+++ tests/Data/Validation/TimeTest.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: jeremiahyoder
+ * Date: 2/22/17
+ * Time: 9:16 AM
+ */
+
+use Neuron\Data\Validation;
+
+class TimeTest extends PHPUnit_Framework_TestCase
+{
+	public function testFail()
+	{
+		$dn = new Validation\Time();
+
+		$dn->setFormat( 'g:i:s A' );
+
+		$this->assertFalse( $dn->isValid( '1:30' ) );
+	}
+
+	public function testPass()
+	{
+		$dn = new Validation\Time();
+
+		$dn->setFormat( 'g:i:s A' );
+
+		$this->assertTrue( $dn->isValid( '1:30:23 PM' ) );
+	}
+}
