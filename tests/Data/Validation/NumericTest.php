52e996bd1606071b69989af7ed2d4726da876a6b
User <sen.hakobyan@gmail.com>
2019-03-26 03:27:40 +0400

    extra namespace removed

--- tests/Data/Validation/NumericTest.php
+++ tests/Data/Validation/NumericTest.php
@@ -10,7 +10,7 @@ class NumericTest extends PHPUnit\Framework\TestCase
 	{
 		parent::setUp();
 		
-		$this->_Validator = new \Neuron\Data\Validation\Numeric();
+		$this->_Validator = new Numeric();
 	}
 
 	public function testPass()
